a81632a6f9a485608e0adc4cd75d0f85aa4eba2c
Thanh Le <lenguyenthanh@hotmail.com>
2026-07-04 11:53:53 +0200

    3.1.0

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -28,7 +28,7 @@ object BuildSettings {
       Tests.Argument(TestFrameworks.Specs2, "showtimes"),
       Tests.Argument(TestFrameworks.JUnit, "-v")
     ),
-    version := "3.0.4"
+    version := "3.1.0"
   )
 
   def playScriptedSettings: Seq[Setting[?]] = Seq(
